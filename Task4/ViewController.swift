854e9b14d3a4ffb7aad1a595565c5a02136bf53f
Roman Krendyasov <krendyasov_rn@mail.ru>
2024-11-10 15:57:49 +0400

    Initial Commit

---
+++ Task4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task4
+//
+//  Created by Роман Крендясов on 10.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
